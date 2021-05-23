e647a97b5a82812f20f6205573ae6f417e1760a4
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2021-05-23 13:29:59 +0700

    edit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,23 @@
 import React from "react";
+import cute from '../cute.png'
 
 function Cards() {
   return (
     <div className="cards">
         <div className="grid-container-card">
-            
+            <div className="grid-item-pic">
+              <img src="" alt="Pokemon" />
+            </div>
+            <div className="grid-item-name">Name</div>
+            <div className="grid-item-add"><button>Add</button></div>
+            <div className="grid-item-label">HP</div>
+            <div className="grid-item-scale"></div>
+            <div className="grid-item-label">STR</div>
+            <div className="grid-item-scale"></div>
+            <div className="grid-item-label">WEAK</div>
+            <div className="grid-item-scale"></div>
+            <div></div>
+            <div className="grid-item-cute"><img src={cute}/></div>
         </div>
     </div>
   );
